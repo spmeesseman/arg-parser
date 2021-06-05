196bcc06f56dce166d0211b6c34cf2be893dd87b
Scott Meesseman <spmeesseman@gmail.com>
2021-06-05 15:45:13 -0400

    chore: v0.0.3

--- lib/argParser.ts
+++ lib/argParser.ts
@@ -1,36 +1,33 @@
 import { env, stderr } from "process"; // eslint-disable-line node/prefer-global/process
-import { isNumeric } from "./lib/util";
+import { isNumeric } from "./util";
 import * as util from "util";
 import gradient from "gradient-string";
 import chalk from "chalk";
-import hideSensitive = require("./lib/hideSensitive");
+import hideSensitive = require("./hideSensitive");
 
-class ArgParser {
-    /*
-     *  Object for parsing command line strings into Python objects.
-     *
-     *  Keyword Arguments:
-     *      - prog -- The name of the program (default: sys.argv[0])
-     *      - usage -- A usage message (default: auto-generated from arguments)
-     *      - description -- A description of what the program does
-     *      - epilog -- Text following the argument descriptions
-     *      - parents -- Parsers whose arguments should be copied into this one
-     *      - formatter_class -- HelpFormatter class for printing help messages
-     *      - prefix_chars -- Characters that prefix optional arguments
-     *      - fromfile_prefix_chars -- Characters that prefix files containing
-     *          additional arguments
-     *      - argument_default -- The default value for all arguments
-     *      - conflict_handler -- String indicating how to handle conflicts
-     *      - add_help -- Add a -h/-help option
-     *      - allow_abbrev -- Allow long options to be abbreviated unambiguously
-     *      - exit_on_error -- Determines whether or not ArgumentParser exits with
-     *          error info when an error occurs
+class ArgParser
+{
+    app: string;
+    banner: string;
+
+    /**
+     * Instantiates an argument parser
+     * @param [options] Parser options
+     * @param [options.app] App name
+     * @param [options.banner]
+     * A banner to be displayed when the console starts up, using chalk and gradient
      */
+    constructor(options: {
+        app: string,
+        banner: string })
+    {
+        if (options) {
+            this.app = options.app;
+            this.banner = options.banner;
+        }
+    }
 
-    // tslint:disable-next-line: no-empty
-    constructor() { }
-
-    _parseArgs(argMap: any)
+    parseArgs(argMap: any)
     {
         return _parseArgs(argMap);
     }
