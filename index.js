0e4e288ad493e76cfcf537c88b48371c910803ab
Scott Meesseman <spmeesseman@gmail.com>
2021-07-01 18:15:33 -0400

    fix: main import points to wrong directory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseArgs = void 0;
-const argParser_1 = require("./lib/argParser");
+const argParser_1 = require("./build/lib/argParser");
 Object.defineProperty(exports, "parseArgs", { enumerable: true, get: function () { return argParser_1.parseArgs; } });
 //# sourceMappingURL=index.js.map
